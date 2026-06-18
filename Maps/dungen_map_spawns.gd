4eae30dec6dbddccdcf269c4f14bd134f80a73a5
JomBom730 <jombom744@gmail.com>
2026-06-18 19:37:26 +0100

    Added the map PinBall and Dungen

---
+++ Maps/dungen_map_spawns.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+@export var size : float
+@export var size_of_hole : float
+enum number_type {POSITIVE, NEGATIVE}
+func get_random_position(maps_scale) -> Vector3:
+	var x : float = randf_range(maps_scale / 2 * size_of_hole, maps_scale / 2 * size)
+	var z : float = randf_range (maps_scale / 2 * size_of_hole, maps_scale / 2 * size)
+	var positive_or_negative = randi_range(1, 2)
+	if positive_or_negative == 2:
+		x = x - (x * 2)
+		z = z - (z * 2)
+	return Vector3(x, 0.25, z)
